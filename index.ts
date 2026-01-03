eaaf43cb22a8ff92c4f3cf95745c46693645f80e
Aaron Menadue <aaron.menadue@gmail.com>
2026-01-03 12:21:03 +1030

    Edit 🚧: Added a check to see if its the only instance that is running

--- index.ts
+++ index.ts
@@ -3,6 +3,21 @@ import oneshotService from "./src/services/oneshots";
 import { cfg, readConfig } from "./src/config";
 import hyprland from "./src/hypr/hyprland";
 
+const pidFile = Bun.file(`${process.env["HOME"]}/.mydaemon.pid`);
+
+if (await pidFile.exists()) {
+	var runningPid = await pidFile.text();
+	var pidCheck = Bun.spawnSync(["ps", "-p", runningPid]);
+	var lines = pidCheck.stdout.toString().split("\n");
+	console.log(lines);
+	if (lines.length > 2) {
+		console.log("mydaemon is already running; closing this instance")
+		process.exit(0);
+	}
+}
+pidFile.write(`${process.pid}`);
+
+
 readConfig("/etc/mydaemon/config.json");
 readConfig(`${process.env["XDG_CONFIG_HOME"] ?? `${process.env["HOME"]}/.config`}/mydaemon/config.json`);
 
